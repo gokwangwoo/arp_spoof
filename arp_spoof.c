2f363900743c1f8dc3fa54a36e0205caf5cb21b3
gokwangwoo <rhrhkddn22@naver.com>
2017-07-31 15:48:26 +0900

    Print my mac and get IP

--- arp_spoof.c
+++ arp_spoof.c
@@ -8,6 +8,18 @@
 #include <stdint.h>
 #include <string.h> 
 #include <stdio.h>
+#include <errno.h>
+#include <netinet/if_ether.h>
+#include <net/if_arp.h>
+#include <stdlib.h>
+#include <net/if.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <netinet/in.h>
+
 struct sniff_ethernet {
 
 	uint8_t h_dest[ETHER_ADDR_LEN]; /* Destination host address */
@@ -46,6 +58,48 @@ struct sniff_ip {
 #define IP_V(ip)	(((ip)->ip_vhl) >> 4)
 
 struct sockaddr_in source,dest;
+int get_mac_by_inf(u_char mac[6], const char *dev){
+	struct ifreq ifr;
+	int fd = socket(AF_INET, SOCK_DGRAM, IPPROTO_IP);
+	
+	strncpy(ifr.ifr_name, dev, IFNAMSIZ-1);
+	
+	if(ioctl(fd, SIOCGIFHWADDR, &ifr) != 0){
+		printf("can't get MAC Address\n");
+		close(fd);
+		return 0;	
+	}	
+
+	for (int i = 0; i < 6; ++i){
+		mac[i] = ifr.ifr_addr.sa_data[i];
+		//printf("%02d", mac[i]);
+	}
+	printf("\n");
+
+	close(fd);
+	return 1;
+}
+int get_ip_by_inf(struct in_addr* ip, const char *dev){
+	struct ifreq ifr;
+	int fd = socket(AF_INET, SOCK_DGRAM, 0);
+	struct sockaddr_in *sin;
+
+	ifr.ifr_addr.sa_family = AF_INET;
+	strncpy(ifr.ifr_name , dev , IFNAMSIZ-1);
+
+	if(ioctl(fd, SIOCGIFADDR, &ifr) != 0){
+		printf("can't get IP Address\n");
+		close(fd);
+		return 0;
+	}
+	 
+	close(fd);
+	
+	sin = (struct sockaddr_in*) &ifr.ifr_addr;
+	*ip = sin->sin_addr;
+
+	return 1;
+}
 
 
 int main(int argc, char *argv[])
@@ -54,9 +108,9 @@ int main(int argc, char *argv[])
 		char *dev;			/* The device to sniff on */
 		char errbuf[PCAP_ERRBUF_SIZE];	/* Error string */
 		struct bpf_program fp;		/* The compiled filter */
-		char filter_exp[] = "port 80";	/* The filter expression */
 		bpf_u_int32 mask;		/* Our netmask */
 		bpf_u_int32 net;		/* Our IP */
+		char filter_exp[] = "port 80";
 		struct pcap_pkthdr *header;	/* The header that pcap gives us */
 		const u_char *Buffer;		/* The actual packet */
 		int size;
@@ -95,6 +149,12 @@ int main(int argc, char *argv[])
 	
 	struct sniff_ethernet* ethernet;
 	struct sniff_arp* arp;
+	struct in_addr my_ip_addr;
+
+	u_char my_mac[6];
+
+	get_mac_by_inf(my_mac, argv[1]);
+	get_ip_by_inf(&my_ip_addr, argv[1]);
 
 	u_char packet[42];
 	packet[0]=255;
